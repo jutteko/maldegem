35a1bca7bc43bf3bf7d4edc8b9f3f184dd293ca3
jutteko <jurgen.francois.laptop@telenet.be>
2021-12-22 20:15:58 +0100

    V2 git commit -m V2 git push origin master live-server git commit -m V2

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
+const maldegemUrl =
+  'https://api.weather.com/v2/pws/observations/current?stationId=IVLAANDE9&format=json&units=m&numericPrecision=decimal&apiKey=da0ebffb6bc04bc68ebffb6bc00bc63d';
+
 const buitenring = document.getElementById('buitenring');
 const temperatuur = document.getElementById('temperatuur');
 const datum = document.getElementById('datum');
 const tijd = document.getElementById('tijd');
 
-const maldegemUrl =
-  'https://api.weather.com/v2/pws/observations/current?stationId=IVLAANDE9&format=json&units=m&numericPrecision=decimal&apiKey=da0ebffb6bc04bc68ebffb6bc00bc63d';
-
 async function getWeatherData(url) {
+  // ophalen en verwerken gegevens
   const response = await fetch(url);
   const data = await response.json();
   const temp = data.observations[0].metric.temp.toFixed(1);
   const date = ('' + data.observations[0].obsTimeLocal).slice(0, 10).split('-');
   const time = ('' + data.observations[0].obsTimeLocal).substring(11, 16);
-  temperatuur.textContent = `${temp}°C`;
+
+  // gegevens bijwerken op UI
   datum.textContent = `${date[2]}-${date[1]}-${date[0]}`;
   tijd.textContent = `${time}`;
+  if (+temperatuur.textContent === temp) {
+    console.log('zelfde temp als een minuut geleden');
+    return;
+  }
+  temperatuur.textContent = `${temp}°C`;
 }
 
+getWeatherData(maldegemUrl);
 setInterval(function () {
   getWeatherData(maldegemUrl);
 }, 60000);
